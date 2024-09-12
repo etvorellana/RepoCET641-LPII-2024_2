90100ea6ab47f837369fd469a6983efed41303bf
andrepardinho <nandre670@gmail.com>
2024-09-12 17:31:37 -0300

    LE_02 revisado

--- Grupo_02/LE_02/exemimg.c
+++ Grupo_02/LE_02/exemimg.c
@@ -3,118 +3,150 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-/*
-// Q01 a Q05
 int main() {
-    srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios
 
-    unsigned char img[480][640];
-
-    // Q02: Testa a função geraImgGreyFull_R
-    unsigned char pixel = geraPixelCinza(0); // Gera um valor de pixel com probabilidade uniforme
-    geraImgGreyFull_R(img, pixel);
-    printf("Pixel [0][0] apos geraImgGreyFull_R: %u\n", img[0][0]);
-    printf("Pixel [639][479] apos geraImgGreyFull_R: %u\n", img[479][639]);
-
-    // Q03: Testa a função geraImgGreyB_R
-    geraImgGreyB_R(img);
-    printf("Pixel [0][0] apos geraImgGreyB_R: %u\n", img[0][0]);
-    printf("Pixel [639][479] apos geraImgGreyB_R: %u\n", img[479][639]);
-
-    // Q04: Testa a função geraImgGreyW_R
-    geraImgGreyW_R(img);
-    printf("Pixel [0][0] apos geraImgGreyW_R: %u\n", img[0][0]);
-    printf("Pixel [639][479] apos geraImgGreyW_R: %u\n", img[479][639]);
-
-    // Q05: Testa a função geraImgGrey
-    int tipo = 1;
-    geraImgGrey(img, tipo);
-    printf("Pixel [0][0] apos geraImgGrey: %u\n", img[0][0]);
-    printf("Pixel [639][479] apos geraImgGrey: %u\n", img[479][639]);
-
-    return 0;
-}
-*/
-
-
-/* 
-// Q06 e Q07
-int main (void) {
-
+    // Inicializa o gerador de números aleatórios
     srand(time(NULL));
 
-    unsigned char img[480][640];
+    // Declaração das variáveis
+    unsigned char linha[LARGURA];
+    unsigned char img[ALTURA][LARGURA];
 
-    for (int i = 0; i < 480; i++) {
-        for (int j = 0; j < 640; j++) {
-            img[i][j] = rand() % 256;
+    // QUESTAO 1: Teste da função geraLinha com tipo = 0 (aleatório)
+    geraLinha(linha, 0);
+    printf("\nPrimeiros 10 valores de linha: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", linha[i]);
+    }
+    printf("\n");
+
+    // QUESTAO 2: Teste da função geraImgGreyFull_R, preenchendo todos os pixeis com o valor predeterminado 100
+    geraImgGreyFull_R(img, 100);
+    printf("Primeiros 10 valores de img apos geraImgGreyFull_R: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", img[0][i]);
+    }
+    printf("\n");
+
+    // QUESTAO 3: Teste da função geraImgGreyB_R, retornando uma imagem preta
+    geraImgGreyB_R(img);
+    printf("Primeiros 10 valores de img apos geraImgGreyB_R: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", img[0][i]);
+    }
+    printf("\n");
+
+    // QUESTAO 4: Teste da função geraImgGreyW_R, retornando uma imagem branca
+    geraImgGreyW_R(img);
+    printf("Primeiros 10 valores de img apos geraImgGreyW_R: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", img[0][i]);
+    }
+    printf("\n");
+
+    // QUESTAO 5: Teste da função geraImgGrey, o parametro altera a probablilidade do tom do pixel.
+    geraImgGrey(img, 1);
+    printf("Primeiros 10 valores de img apos geraImgGrey: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", img[0][i]);
+    }
+    printf("\n");
+
+    // QUESTAO 6: Teste da função pixelMax_R, retorna o pixel de maior intensidade
+    // Preenche a matriz com um valor constante e coloca um valor maior em uma posição específica
+    for (int i = 0; i < ALTURA; i++) {
+        for (int j = 0; j < LARGURA; j++) {
+            img[i][j] = 100;
         }
     }
 
+    img[ALTURA - 1][LARGURA - 1] = 200;
     int pixel_max = pixelMax_R(img);
+
+    printf("\nPixel de maior intensidade: %d\n", pixel_max);
+
+    // QUESTAO 7: Teste da função pixelMin_R, retorna o pixel de menor intensidade
+    // Preenche a matriz com um valor constante (reutiliza o for da Q06) e coloca um valor menor em uma posição específica
+    img[ALTURA - 1][LARGURA - 1] = 50;     
     int pixel_min = pixelMin_R(img);
 
-    printf("Pixel de maior intensidade: %d\n", pixel_max);
     printf("Pixel de menor intensidade: %d\n", pixel_min);
 
-    return 0;
-}
-*/
+    // QUESTAO 08, 09 e 10: Teste das funcoes somaPorLinhas_R, somaPorColunas_R e somaPorTotal_R
+    int somaL[ALTURA];
+    int somaC[LARGURA];
+    int somaTotal;
 
-
-/*
-// Q11 a Q13
-int main() {
-    // Declara uma imagem com dimensões 480x640
-    unsigned char img[480][640];
-
-    // Preenche a imagem com valores de exemplo (aqui, todos os pixels com valor 128)
-    for(int i = 0; i < 480; i++) {
-        for(int j = 0; j < 640; j++) {
-            img[i][j] = 128; // Pode preencher com outros valores ou carregar uma imagem real
+    // Inicializa a imagem com valores para teste
+    for (int i = 0; i < ALTURA; i++) {
+        for (int j = 0; j < LARGURA; j++) {
+            img[i][j] = 1; // Preenche com 1
         }
     }
 
-    unsigned char intensidade = 128; // Intensidade de exemplo
+    somaPorLinhas_R(img, somaL);
+    somaPorColunas_R(img, somaC);
+    somaTotal = somaPorTotal_R(img);
+
+    // Verificação
+    int erroLinha = 0;
+    for (int i = 0; i < ALTURA; i++) {
+        if (somaL[i] != LARGURA) {
+            erroLinha = 1; 
+            break;
+        }
+    }
+
+    if (erroLinha) printf("\nErro nas somas das linhas.\n");
+    else printf("\nSoma das linhas correta.\n");
+
+    int erroColuna = 0;
+    for (int i = 0; i < LARGURA; i++) {
+        if (somaC[i] != ALTURA) {
+            erroColuna = 1;
+            break;
+        }
+    }
+
+    if(erroColuna) printf("Erro nas somas das colunas.\n");
+    else printf("Soma das colunas correta.\n");
+
+    if (somaTotal != ALTURA * LARGURA) printf("\nErro na soma total.\n");
+    else printf("Soma total: %d (Resultado correto)\n", somaTotal);
+
+    // QUESTOES 11, 12 e 13: Preenche a imagem com valores de exemplo (aqui, todos os pixels com valor 128)
+    for(int i = 0; i < ALTURA; i++) {
+        for(int j = 0; j < LARGURA; j++) {
+            img[i][j] = 128;
+        }
+    }
+
+    unsigned char intensidade = 128; // Intensidade teste
+    img[0][0] = intensidade - 1; // Altera p/ um pixel abaixo da intensidade teste
+    img[0][1] = intensidade + 1; // Altera p/ um pixel acima da intensidade teste
+
 
     // Testa a função para contar pixels com intensidade específica
-    int resultado = quantosPixelsNaInt_R(img, intensidade);
-    printf("Pixels com intensidade %d: %d\n", intensidade, resultado);
+    int resultado1 = quantosPixelsNaInt_R(img, intensidade);
+    printf("\nPixels com intensidade %d: %d", intensidade, resultado1);
+    
+    if (resultado1 == (ALTURA * LARGURA) - 2) printf(" (Resultado correto)\n");
+    else printf(" (Resultado incorreto)\n");
 
     // Testa a função para contar pixels abaixo de uma intensidade
-    resultado = quantosPixelsAbaixoDeInt_R(img, intensidade);
-    printf("Pixels com intensidade menor que %d: %d\n", intensidade, resultado);
+    int resultado2 = quantosPixelsAbaixoDeInt_R(img, intensidade);
+    printf("Pixels com intensidade menor que %d: %d", intensidade, resultado2);
+
+    if (resultado2 == 1) printf(" (Resultado correto)\n");
+    else printf(" (Resultado incorreto)\n");
 
     // Testa a função para contar pixels acima de uma intensidade
-    resultado = quantosPixelsAcimaDeInt_R(img, intensidade);
-    printf("Pixels com intensidade maior que %d: %d\n", intensidade, resultado);
+    int resultado3 = quantosPixelsAcimaDeInt_R(img, intensidade);
+    printf("Pixels com intensidade maior que %d: %d", intensidade, resultado3);
+
+    if (resultado3 == 1) printf(" (Resultado correto)\n");
+    else printf(" (Resultado incorreto)\n");
+
 
     return 0;
 }
-*/
-
-
-/*
-// Q14
-int main() {
-
-unsigned char img[480][640];
-int Pmax, Pmin, QntPmax, QntPmin;
-float search = -0.5;
-
-  Pmax = pixelMax_R(img);
-  Pmin = pixelMin_R(img);
-  QntPmax = quantosPixelsNaInt_R(img, Pmax);
-  QntPmin = quantosPixelsNaInt_R(img, Pmin);
-
-  unsigned char PE;
-
-  if(search == -0.5)
-  PE = ((QntPmax * Pmax) + (QntPmin * Pmin) / (QntPmax + QntPmin));
-
-  pontoEquilibrio(img, PE);
-
-  return 0;
-}
-*/
--- Grupo_02/LE_02/procimg.h
+++ Grupo_02/LE_02/procimg.h
@@ -1,60 +1,61 @@
 #ifndef PROCIMG_H
 #define PROCIMG_H
 
+// Definição das dimensões da imagem
+#define ALTURA 48  // Altura padrão
+#define LARGURA 64 // Largura padrão
+
 //Q01:
 unsigned char geraPixelCinza(int tipo);
 
-void geraLinhaR(unsigned char lin[640], int tipo, int index);
-void geraLinha(unsigned char lin[640], int tipo);
+void geraLinhaR(unsigned char lin[LARGURA], int tipo, int index);
+void geraLinha(unsigned char lin[LARGURA], int tipo);
 
-// FUNÇÃO AUX P/ Q02, Q03, Q04
-void preencheRecursivo(unsigned char img[480][640], unsigned char pixel, int i, int j);
+// Função AUX P/ Q02, Q03, Q04
+void preencheRecursivo(unsigned char img[ALTURA][LARGURA], unsigned char pixel, int i, int j);
 
 //Q02
-void geraImgGreyFull_R(unsigned char img[480][640], unsigned char pixel);
+void geraImgGreyFull_R(unsigned char img[ALTURA][LARGURA], unsigned char pixel);
 
 //Q03
-void geraImgGreyB_R(unsigned char img[480][640]);
+void geraImgGreyB_R(unsigned char img[ALTURA][LARGURA]);
 
 //Q04
-void geraImgGreyW_R(unsigned char img[480][640]);
+void geraImgGreyW_R(unsigned char img[ALTURA][LARGURA]);
 
 //Q05
-void geraImgGrey_R(unsigned char img[480][640], int tipo, int i, int j);
-void geraImgGrey(unsigned char img[480][640], int tipo);
+void geraImgGrey_R(unsigned char img[ALTURA][LARGURA], int tipo, int i, int j);
+void geraImgGrey(unsigned char img[ALTURA][LARGURA], int tipo);
 
 //Q06
-int pixelMax_R_aux(unsigned char img[480][640], int linha, int coluna, int valorMax);
-int pixelMax_R(unsigned char img[480][640]);
+int pixelMax_R_aux(unsigned char img[ALTURA][LARGURA], int linha, int coluna, int valorMax);
+int pixelMax_R(unsigned char img[ALTURA][LARGURA]);
 
 //Q07
-int pixelMin_R_aux(unsigned char img[480][640], int linha, int coluna, int valorMin);
-int pixelMin_R(unsigned char img[480][640]);
+int pixelMin_R_aux(unsigned char img[ALTURA][LARGURA], int linha, int coluna, int valorMin);
+int pixelMin_R(unsigned char img[ALTURA][LARGURA]);
 
 //Q08
-void somaPorLinhas_R_Aux(unsigned char img[480][640], int soma[480], int linha, int coluna, int somaLinha);
-void somaPorLinhas_R(unsigned char img[480][640], int soma[480]);
+void somaPorLinhas_R_Aux(unsigned char img[ALTURA][LARGURA], int soma[ALTURA], int linha, int coluna, int somaLinha);
+void somaPorLinhas_R(unsigned char img[ALTURA][LARGURA], int soma[ALTURA]);
 
 //Q09
-void somaPorColunas_R_Aux(unsigned char img[480][640], int soma[640], int linha, int coluna, int somaColuna);
-void somaPorColunas_R(unsigned char img[480][640], int soma[640]);
+void somaPorColunas_R_Aux(unsigned char img[ALTURA][LARGURA], int soma[LARGURA], int linha, int coluna, int somaColuna);
+void somaPorColunas_R(unsigned char img[ALTURA][LARGURA], int soma[LARGURA]);
 
 //Q10
-int somaPorTotal_R_Aux(unsigned char img[480][640], int linha, int coluna, int somaTotal);
-int somaPorTotal_R(unsigned char img[480][640]);
+int somaPorTotal_R_Aux(unsigned char img[ALTURA][LARGURA], int linha, int coluna, int somaTotal);
+int somaPorTotal_R(unsigned char img[ALTURA][LARGURA]);
 
 //Q11
-int quantosPixelsNaInt_R_Aux(unsigned char img[480][640], unsigned char intensidade, int lin, int col);
-int quantosPixelsNaInt_R(unsigned char img[480][640], unsigned char intensidade);
+int quantosPixelsNaInt_R_Aux(unsigned char img[ALTURA][LARGURA], unsigned char intensidade, int lin, int col);
+int quantosPixelsNaInt_R(unsigned char img[ALTURA][LARGURA], unsigned char intensidade);
 
 //Q12
-int quantosPixelsAbaixoDeInt_R_Aux(unsigned char img[480][640], unsigned char intensidade, int lin, int col);
-int quantosPixelsAbaixoDeInt_R(unsigned char img[480][640], unsigned char intensidade);
+int quantosPixelsAbaixoDeInt_R_Aux(unsigned char img[ALTURA][LARGURA], unsigned char intensidade, int lin, int col);
+int quantosPixelsAbaixoDeInt_R(unsigned char img[ALTURA][LARGURA], unsigned char intensidade);
 
 //Q13
-int quantosPixelsAcimaDeInt_R(unsigned char img[480][640], unsigned char intensidade);
-
-//Q14
-unsigned char pontoEquilibrio(unsigned char img[480][640], unsigned char intt);
+int quantosPixelsAcimaDeInt_R(unsigned char img[ALTURA][LARGURA], unsigned char intensidade);
 
 #endif
